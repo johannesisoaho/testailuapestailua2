8cd1885f2166fddef9d79f9f41e3df569cc94466
Johannes Isoaho <johannes.isoaho@cs.tamk.fi>
2017-02-22 16:03:13 +0200

    vaihdoin taustaväriä! toivottavasti tykkäät :--) pitää vielä kysyä samilta että mitä mieltä se on koska se kumminkin päättää grafiikoista.

--- desktop/src/fi/tamk/testailua/pestailua/desktop/DesktopLauncher.java
+++ desktop/src/fi/tamk/testailua/pestailua/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import fi.tamk.testailua.pestailua.TestailuaPestailua;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 800;
+        config.height = 480;
+        config.title = "paras peli :D";
 		new LwjglApplication(new TestailuaPestailua(), config);
 	}
 }
