5f84c30db66b51a458b0b902ded99f490c16c184
Johannes Isoaho <johannes.isoaho@cs.tamk.fi>
2017-02-22 15:20:28 +0200

    Initial commit

---
+++ desktop/src/fi/tamk/testailua/pestailua/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fi.tamk.testailua.pestailua.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fi.tamk.testailua.pestailua.TestailuaPestailua;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TestailuaPestailua(), config);
+	}
+}
